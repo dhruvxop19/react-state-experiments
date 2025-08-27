596b8947f5eeb438469e477955a8325482072b90
dhruvxop19 <dhruvxop19@gmail.com>
2025-08-27 07:27:42 +0530

    first commit- made four components

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+function App() {
+  return <div>
+
+    <LightBulb />
+
+  </div>
+}
+
+function LightBulb() {
+  return <div>
+    <BulbState />
+    <ToggleBulbState />
+  </div>
+}
+
+function BulbState() {
+  return <div>
+
+  </div>
+}
+
+function ToggleBulbState() {
+  return <div>
+    <button>Toggle the bulb</button>
+  </div>
+}
+export default App
